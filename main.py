842f1727b19f1ad7487a516d7bf2156debbe9509
stephen911 <stephendappah1@gmail.com>
2022-03-21 14:43:39 +0000

    'new'

--- main.py
+++ main.py
@@ -19,4 +19,6 @@ def gameframe(xmove):
 
             mylist1 = "As I sit in my room late at night, staring at the computer screen, I decide it would be a good idea to create this text. There isn't much meaning to it, other than to get some simple practice."
                 mylist2 = "An appraisal invites the bond. The wine breaks an instructed employer underneath an economics. The domestic exits near a servant. An ancient comic sweeps. A fashioned autumn stumbles inside a criminal"
-                
+                    mylist3 = "Lines of weeds criss crossed the cracked parking lot of the Seashell Motor Courts. The flaking paint on the buildings had chalked to a pastel pink on walls covered with graffiti. Many of the windows had been smashed out. Where the sign had been, atop rusting steel posts, only the metal outline of a seashell remained."
+
+                    
