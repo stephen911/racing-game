68d89839e7eb6c0a66e4085d9d1c6268276c7ac3
stephen911 <stephendappah1@gmail.com>
2022-03-21 14:47:29 +0000

    'new'

--- main.py
+++ main.py
@@ -48,4 +48,5 @@ def gameframe(xmove):
                                                                                                                                         text3 = "Lines of weeds criss crossed the cracked parking lot of the Seashell Motor Courts. The flaking paint on the \nbuildings had chalked to a pastel pink on walls covered with graffiti. Many of the windows had been \nsmashed out. Where the sign had been, atop rusting steel posts, only the metal outline of a seashell remained."
                                                                                                                                                 w.create_text(400, 180, text=text3, font=("Comic Sans MS", 11))
 
-                                                                                                                                                
+                                                                                                                                                    ~ road and cars
+                                                                                                                                                    
