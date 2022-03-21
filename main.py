0168704124043ea97231d2ecd117869e24831c31
stephen911 <stephendappah1@gmail.com>
2022-03-21 14:46:39 +0000

    'new'

--- main.py
+++ main.py
@@ -42,4 +42,5 @@ def gameframe(xmove):
                                                                                         text1 = "As I sit in my room late at night, staring at the computer screen, I decide it would be a good idea to \ncreate this text. There isn't much meaning to it, other than to get some simple practice."
                                                                                                 w.create_text(410, 180, text=text1, font=("Comic Sans MS", 11))
                                                                                                     if choice == mylist2:
-                                                                                                        
+                                                                                                                text2 = "An appraisal invites the bond. The wine breaks an instructed employer underneath an economics. \nThe domestic exits near a servant. An ancient comic sweeps. A fashioned autumn stumbles inside \na criminal"
+                                                                                                                
