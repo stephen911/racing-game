fb4b8421818de7e3ef899f397452b399e6e4bdec
stephen911 <stephendappah1@gmail.com>
2022-03-21 14:44:11 +0000

    'new'

--- main.py
+++ main.py
@@ -24,4 +24,5 @@ def gameframe(xmove):
                         ~ select random paragraph
                             choice = random.choice([mylist1, mylist2, mylist3])
 
-                            
+                                app.geometry("800x400")
+                                
